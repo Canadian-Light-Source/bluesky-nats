fa7fd4fa5c6fc7df1c1b8206b00fdad169c3f10a
Niko Kivel <niko.kivel@lightsource.ca>
2024-09-19 10:27:27 -0600

    one more for the linter

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
+from bluesky.run_engine import RunEngine
+
 from bluesky_nats.nats_client import NATSClientConfig
 from bluesky_nats.nats_publisher import CoroutineExecutor, NATSPublisher
 
-from bluesky.run_engine import RunEngine
-
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     RE = RunEngine({})
     config = NATSClientConfig()
-    print(config)
-
     nats_publisher = NATSPublisher(
-    client_config=config,
-    executor=CoroutineExecutor(RE.loop),
-    subject_factory="events.DAQ.prj00SC00000",
-)
+        client_config=config,
+        executor=CoroutineExecutor(RE.loop),
+        subject_factory="events.test.nats.publisher",
+    )
