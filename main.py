b8ec043ea5ec41d6fc51cff69cd7694c2b332fee
Niko Kivel <niko.kivel@lightsource.ca>
2024-09-24 07:46:20 -0600

    asyncio tests basics

--- main.py
+++ main.py
@@ -21,15 +21,17 @@ if __name__ == "__main__":
         .from_file("examples/config/config.json")   # load default configuration from file from JSON
         .from_file("examples/config/cluster.yaml") # overwrite ALL fields from another file, but YAML
         .from_file("examples/config/cluster.toml") # yet again, TOML this timed not that this makes any sense
-        .set("max_reconnect_attempts", value=20)    # this sets a single field manually
+        .set("max_reconnect_attempts", value=2)    # this sets a single field manually
         .set_callback("error_cb", bluesky_nats.callbacks.error_callback)    # register a callback from the module
         .set_callback("user_jwt_cb", lambda: print("user_jwt_callback"))    # register a callback from a lambda
         .build()    # put it all together
     )
 
+    # print the configuration
     for key, value in asdict(config).items():
         print(f"{key}: {value}")
 
+    # This will most likely fail because the cluster nodes will not be available.
     nats_publisher = NATSPublisher(
         client_config=config,
         executor=CoroutineExecutor(RE.loop),
